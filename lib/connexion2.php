26571c64ac866b20ff35cc64319776b732417ff8
Rifat <rifat@MacBook-Pro-de-Rifat.local>
2017-01-10 15:04:45 +0100

    Supression du fichier de connexion à la base de données

--- lib/connexion2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-	$bdd = new PDO('mysql:host=db654192383.db.1and1.com;dbname=db654192383;charset=utf8', 'dbo654192383', 'canauxdediscussion');
-	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-	die('Erreur : ' . $e->getMessage());
-}
