8dbc7a7f6911285d702528eda6905d81a0e28f8b
Rifat95 <islamrifat666@gmail.com>
2017-01-20 12:36:43 +0100

    Mise à jour du tutoriel

--- vues/liste-diffusions.php
+++ vues/liste-diffusions.php
@@ -37,7 +37,7 @@
 <h3>Programmer une diffusion</h3>
 
 <form method="post" action="liste-diffusions.php">
-	<p><a href="tuto.pdf">Comment diffuser via Dailymotion ?</a></p>
+	<p><a href="tuto.html">Comment diffuser via Dailymotion ?</a></p>
 	<p>
 		<label for="titre">Titre</label>
 		<input type="text" name="titre" id="titre" required />
